d185da7ae399f35ad465a79507117dfbba8d5c50
terw-dan <danny@yoast.com>
2016-10-11 13:43:51 +0200

    Gets german participle

---
+++ js/values/SentencePart.js
@@ -0,0 +1,24 @@
+var defaults = require( "lodash/defaults" );
+
+var SentencePart = function( sentencePartText, locale ) {
+	this._sentencePartText = sentencePartText
+	this._locale = locale;
+	this._isPassive = false;
+};
+
+/**
+ *
+ * @returns {*}
+ */
+SentencePart.prototype.getSentencePartText = function() {
+	return this._sentencePartText;
+};
+
+SentencePart.prototype.isPassive = function() {
+	return this._isPassive;
+};
+
+// todo add check if already passive.
+SentencePart.prototype.setPassive = function( passive ) {
+	this._isPassive = passive;
+};
--- spec/researches/german/passivevoice-german/getParticipleSpec.js
+++ spec/researches/german/passivevoice-german/getParticipleSpec.js
@@ -1,33 +1,33 @@
-var matchParticiplesPerWord = require("../../../../js/researches/german/passivevoice-german/matchParticiplesPerWord.js")();
+var getParticiples = require("../../../../js/researches/german/passivevoice-german/getParticiples.js")();
 
 describe("Test for matching German participles", function(){
 	it("returns matched participles with 'ge' at the beginning.", function(){
 		var sentence = "Wir haben gewandert";
-		expect( matchParticiplesPerWord( sentence ) ).toEqual( [ "gewandert" ] );
+		expect( getParticiples( sentence ) ).toEqual( [ "gewandert" ] );
 	});
 
 	it("returns matched participles with 'be' in the middle.", function(){
 		var sentence = "Es wird vorbereitet";
-		expect( matchParticiplesPerWord( sentence ) ).toEqual( [ "vorbereitet" ] );
+		expect( getParticiples( sentence ) ).toEqual( [ "vorbereitet" ] );
 	});
 
 	it("returns matched participles with 'ge' in the middle.", function(){
 		var sentence = "Ich habe das aufgewacht";
-		expect( matchParticiplesPerWord( sentence ) ).toEqual( [ "aufgewacht" ] );
+		expect( getParticiples( sentence ) ).toEqual( [ "aufgewacht" ] );
 	});
 
 	it("returns matched participles with 'er' at the beginning.", function(){
 		var sentence = "Sie haben erzählt";
-		expect( matchParticiplesPerWord( sentence ) ).toEqual( [ "erzählt" ] );
+		expect( getParticiples( sentence ) ).toEqual( [ "erzählt" ] );
 	});
 
 	it("returns matched participles with 'iert'.", function(){
 		var sentence = "Das wurde probiert";
-		expect( matchParticiplesPerWord( sentence ) ).toEqual( [ "probiert" ] );
+		expect( getParticiples( sentence ) ).toEqual( [ "probiert" ] );
 	});
 
 	it("returns an empty array when there is no participle", function(){
 		var sentence = "Yahoo prüfte seitdem den Sachverhalt.";
-		expect( matchParticiplesPerWord( sentence ) ).toEqual( [ ] );
+		expect( getParticiples( sentence ) ).toEqual( [ ] );
 	});
 });
