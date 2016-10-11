9307f78dd4f9e94cec1c7f747dad09d9f8866c14
IreneStr <irene@yoast.com>
2016-10-11 14:59:57 +0200

    Adds more extensive specs for getParticiple

--- spec/researches/german/passivevoice-german/getParticipleSpec.js
+++ spec/researches/german/passivevoice-german/getParticipleSpec.js
@@ -2,40 +2,53 @@ var getParticiples = require("../../../../js/researches/german/passivevoice-germ
 
 describe("Test for matching German participles", function(){
 	it("returns matched participles with 'ge' at the beginning.", function(){
-		var sentence = "Wir haben gewandert";
+		var sentence = "Jetzt wird der Mann ins Krankenhaus gebracht.";
 		var foundParticiples = getParticiples( sentence );
-		expect( foundParticiples[ 0 ].getParticiple() ).toEqual( "gewandert" );
+		expect( foundParticiples[ 0 ].getParticiple() ).toEqual( "gebracht" );
 		expect( foundParticiples[ 0 ].getType() ).toEqual( "ge at beginning" );
-		expect( foundParticiples[ 0 ].getSentencePart() ).toEqual( "Wir haben gewandert" );
-		expect( foundParticiples[ 0 ].getType() ).toEqual( "ge at beginning" );
+		expect( foundParticiples[ 0 ].getSentencePart() ).toEqual( "Jetzt wird der Mann ins Krankenhaus gebracht." );
+		expect( foundParticiples[ 0 ].getAuxiliary() ).toEqual( "wird" );
+		expect( foundParticiples[ 0 ].determinesSentencePartIsPassive() ).toEqual( true );
 	});
 
 	it("returns matched participles with 'be' in the middle.", function(){
-		var sentence = "Es wird vorbereitet";
+		var sentence = "Es wird vorbereitet.";
 		var foundParticiples = getParticiples( sentence );
 		expect( foundParticiples[ 0 ].getParticiple() ).toEqual( "vorbereitet" );
 		expect( foundParticiples[ 0 ].getType() ).toEqual( "er/ver/ent/be/zer/her in the middle" );
+		expect( foundParticiples[ 0 ].getSentencePart() ).toEqual( "Es wird vorbereitet." );
+		expect( foundParticiples[ 0 ].getAuxiliary() ).toEqual( "wird" );
+		expect( foundParticiples[ 0 ].determinesSentencePartIsPassive() ).toEqual( true );
 	});
 
 	it("returns matched participles with 'ge' in the middle.", function(){
-		var sentence = "Ich habe das aufgewacht";
+		var sentence = "Dem Verletzten wurde ein Verband angelegt.";
 		var foundParticiples = getParticiples( sentence );
-		expect( foundParticiples[ 0 ].getParticiple() ).toEqual( "aufgewacht" );
+		expect( foundParticiples[ 0 ].getParticiple() ).toEqual( "angelegt" );
 		expect( foundParticiples[ 0 ].getType() ).toEqual( "ge in the middle" );
+		expect( foundParticiples[ 0 ].getSentencePart() ).toEqual( "Dem Verletzten wurde ein Verband angelegt." );
+		expect( foundParticiples[ 0 ].getAuxiliary() ).toEqual( "wurde" );
+		expect( foundParticiples[ 0 ].determinesSentencePartIsPassive() ).toEqual( true );
 	});
 
 	it("returns matched participles with 'er' at the beginning.", function(){
-		var sentence = "Sie haben erzählt";
+		var sentence = "Das Passiv wurde uns erklärt.";
 		var foundParticiples = getParticiples( sentence );
-		expect( foundParticiples[ 0 ].getParticiple() ).toEqual( "erzählt" );
+		expect( foundParticiples[ 0 ].getParticiple() ).toEqual( "erklärt" );
 		expect( foundParticiples[ 0 ].getType() ).toEqual( "er/ver/ent/be/zer/her at beginning" );
+		expect( foundParticiples[ 0 ].getSentencePart() ).toEqual( "Das Passiv wurde uns erklärt." );
+		expect( foundParticiples[ 0 ].getAuxiliary() ).toEqual( "wurde" );
+		expect( foundParticiples[ 0 ].determinesSentencePartIsPassive() ).toEqual( true );
 	});
 
 	it("returns matched participles with 'iert'.", function(){
-		var sentence = "Das wurde probiert";
+		var sentence = "Das wurde probiert.";
 		var foundParticiples = getParticiples( sentence );
 		expect( foundParticiples[ 0 ].getParticiple() ).toEqual( "probiert" );
 		expect( foundParticiples[ 0 ].getType() ).toEqual( "iert at the end" );
+		expect( foundParticiples[ 0 ].getSentencePart() ).toEqual( "Das wurde probiert." );
+		expect( foundParticiples[ 0 ].getAuxiliary() ).toEqual( "wurde" );
+		expect( foundParticiples[ 0 ].determinesSentencePartIsPassive() ).toEqual( true );
 	});
 
 	it("returns an empty array when there is no participle", function(){
