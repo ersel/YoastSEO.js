1db8c889ff7d7a8fab3ea679813db1fe340fc45d
terw-dan <danny@yoast.com>
2016-10-11 16:57:09 +0200

    add docs

--- js/researches/german/GermanSentencePart.js
+++ js/researches/german/GermanSentencePart.js
@@ -2,12 +2,21 @@ var SentencePart = require( "../../values/SentencePart.js" );
 
 var getParticiples = require( "./passivevoice-german/getParticiples.js" );
 
+/**
+ * Creates a German specific sentence part.
+ * @param {string} sentencePartText The text from the sentence part.
+ * @constructor
+ */
 var GermanSentencePart = function( sentencePartText ) {
 	SentencePart.call( this, sentencePartText );
 };
 
 require( "util" ).inherits( GermanSentencePart, SentencePart );
 
+/**
+ * Returns the participles found in the sentence part..
+ * @returns {Array|*}
+ */
 GermanSentencePart.prototype.getParticiples = function() {
 	return getParticiples( this.getSentencePartText() );
 };
